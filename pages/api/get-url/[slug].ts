3a5f6c02b967cb6158d4fa8e3336a598d484e841
Chris Barber-Riley <chris.barberriley@gmail.com>
2022-06-20 20:57:22 +0100

    Add: working redirect

--- pages/api/get-url/[slug].ts
+++ pages/api/get-url/[slug].ts
@@ -5,14 +5,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
   console.log("slug", slug);
 
-  const { data } = await supabase
+  // Should never occur as route would go to index and not this page
+  if (!slug) {
+    res.status(400).json({ error: "Missing slug" });
+    return;
+  }
+
+  const { data, error } = await supabase
     .from("links")
     .select("*")
     .eq("slug", slug)
     .limit(1)
     .single();
 
-  console.log("Data", data);
+  if (error) {
+    res.status(404).json({ message: "Not found" });
+    return;
+  }
 
-  res.json({ message: "Hello world" });
+  return res.redirect(data.url);
 };
